a7ea8789311fe8cb3396e3843421966a640dd1f2
svbrause <svbrause@gmail.com>
2016-07-24 20:21:37 -0400

    Bonus Assignment

--- Bonus Assignment.playground/Contents.swift
+++ Bonus Assignment.playground/Contents.swift
@@ -8,7 +8,7 @@ let months = [0, 31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31]
 
 func julianDate(year: Int, month: Int, day: Int) -> Int {
     let numYears = Array(1900..<(year)).map({(number:Int) -> Int in return isLeap(number) ? 366:365}).reduce(0, combine: +)
-    let numMonths = months[0..<month].reduce(0, combine: +)
+    let numMonths = months[0..<month].map({(number:Int) -> Int in return number==28&&isLeap(year) ? 29:number}).reduce(0, combine: +)
     return numYears + numMonths + day
 }
 
@@ -19,6 +19,7 @@ julianDate(1901, month: 1, day: 1)
 julianDate(1901, month: 1, day: 1) - julianDate(1900, month: 1, day: 1)
 julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
 
+julianDate(2000, month: 12, day: 31)
 isLeap(1960)
 isLeap(1900)
 isLeap(2000)
